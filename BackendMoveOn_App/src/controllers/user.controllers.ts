ff3586705558f18c7d394ffa27611c66fdefe140
Mikensonthomas142 <mikensonthomas2@gmail.com>
2025-12-16 23:43:07 -0300

    Hash

--- BackendMoveOn_App/src/controllers/user.controllers.ts
+++ BackendMoveOn_App/src/controllers/user.controllers.ts
@@ -3,14 +3,17 @@ import type { Users } from "../types/typesData.js";
 import { userSchema } from "../models/schema/schemaData.js";
 import { db } from "../models/db/connection.js";
 import sql from "@databases/sql";
+import { passwordHash } from "../utils/auth.js";
 
 export async function inserDataUsersControllers(req: Request, res: Response) {
     try {
         const parsed: Users = userSchema.parse(req.body);
 
+        const hash = await passwordHash(parsed.password)
+
         const result = await db.query(sql`
             INSERT INTO users (name, cpf, sex, email, phone, password)
-            VALUES (${parsed.name}, ${parsed.cpf}, ${parsed.sex}, ${parsed.email}, ${parsed.phone}, ${parsed.password})
+            VALUES (${parsed.name}, ${parsed.cpf}, ${parsed.sex}, ${parsed.email}, ${parsed.phone}, ${hash})
             RETURNING id, name, email, created_at
         `);
 
